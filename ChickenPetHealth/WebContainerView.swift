0a27fdec990995b647e7799a8dc3f597f43ee4bc
1exar <73644963+1exar@users.noreply.github.com>
2025-12-05 16:40:20 +0200

    remove loading

--- ChickenPetHealth/WebContainerView.swift
+++ ChickenPetHealth/WebContainerView.swift
@@ -3,32 +3,19 @@ import WebKit
 
 struct WebContainerView: View {
     let url: URL
-    @State private var isLoading = true
 
     var body: some View {
-        ZStack {
-            WebView(url: url, isLoading: $isLoading)
-                .ignoresSafeArea()
-
-            if isLoading {
-                ProgressView("Loading...")
-                    .padding(16)
-                    .background(Color.black.opacity(0.6))
-                    .cornerRadius(12)
-                    .foregroundColor(.white)
-            }
-        }
+        WebView(url: url)
+            .ignoresSafeArea()
     }
 }
 
 private struct WebView: UIViewRepresentable {
     let url: URL
-    @Binding var isLoading: Bool
 
     func makeUIView(context: Context) -> WKWebView {
         let configuration = WKWebViewConfiguration()
         let webView = WKWebView(frame: .zero, configuration: configuration)
-        webView.navigationDelegate = context.coordinator
         webView.allowsBackForwardNavigationGestures = true
         webView.scrollView.contentInsetAdjustmentBehavior = .never
         return webView
@@ -40,31 +27,4 @@ private struct WebView: UIViewRepresentable {
         }
     }
 
-    func makeCoordinator() -> Coordinator {
-        Coordinator(isLoading: $isLoading)
-    }
-
-    final class Coordinator: NSObject, WKNavigationDelegate {
-        @Binding var isLoading: Bool
-
-        init(isLoading: Binding<Bool>) {
-            _isLoading = isLoading
-        }
-
-        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-            isLoading = true
-        }
-
-        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-            isLoading = false
-        }
-
-        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-            isLoading = false
-        }
-
-        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
-            isLoading = false
-        }
-    }
 }
